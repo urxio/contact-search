d51bbb318d0258d912760b0788370cc7fbc4c054
urxio <bnash058@gmail.com>
2026-08-22 13:41:40 -0400

    Rename congregation address upload

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -966,7 +966,7 @@ function OtmPanel() {
             <svg className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a2 2 0 002 2h12a2 2 0 002-2v-1M12 12V4m0 0L8 8m4-4l4 4" />
             </svg>
-            {fileName ? "Change file" : "Upload OTM Excel"}
+            {fileName ? "Change file" : "Upload congregation addresses"}
           </label>
 
           {/* File name pill */}
